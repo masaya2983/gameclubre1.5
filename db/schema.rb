5744095e935c49c9f71f31ccce4ef71e6d934120
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-15 09:00:45 +0000

    コメント

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_085110) do
+ActiveRecord::Schema.define(version: 2023_06_15_085837) do
+
+  create_table "coments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "game_i"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "title"
